af207b5a9b866d25f8862b65fd18f2907af315fc
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-01-26 01:04:59 +0530

    updated

--- frontend/src/Components/test/ComingSoon/ComingSoon.jsx
+++ frontend/src/Components/test/ComingSoon/ComingSoon.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import VideoBg from "./video.mp4";
 
 const ComingSoon = () => {
@@ -11,7 +11,9 @@ const ComingSoon = () => {
 
     if (difference > 0) {
       const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
       const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
@@ -19,14 +21,14 @@ const ComingSoon = () => {
         days,
         hours,
         minutes,
-        seconds
+        seconds,
       };
     } else {
       return {
         days: 0,
         hours: 0,
         minutes: 0,
-        seconds: 0
+        seconds: 0,
       };
     }
   };
@@ -43,11 +45,52 @@ const ComingSoon = () => {
 
   return (
     <div className="coming-soon-container relative min-h-screen overflow-hidden">
-      <video className='h-full w-full object-cover absolute inset-0' src={VideoBg} autoPlay muted loop></video>
+      <Helmet>
+        <title>Daily Current Affairs/ कर्रेंट अफेयर्स</title>
+        <meta
+          name="description"
+          content="Current Affairs for UPSC, BPSC, बिहार दारोगा, SI, BSSC, Railway, JSSC, SSC, BANKING, Defence.."
+        />
+        <link rel="canonical" href={canonicalUrl} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="" />
+        <meta
+          property="og:description"
+          content="Current Affairs for UPSC, BPSC, बिहार दारोगा, SI, BSSC, Railway, JSSC, SSC, BANKING, Defence.."
+        />
+        <meta
+          property="og:image"
+          content="https://unchiudaanclasses.com/uchiudan.png"
+        />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Unchiudan Tests" />
+        <meta
+          name="twitter:description"
+          content="Current Affairs for UPSC, BPSC, बिहार दारोगा, SI, BSSC, Railway, JSSC, SSC, BANKING, Defence.."
+        />
+        <meta
+          name="twitter:image"
+          content="https://unchiudaanclasses.com/uchiudan.png"
+        />
+
+        <meta name="author" content="Anuraj kumar, ishu singh, @ImKKingshuk" />
+      </Helmet>
+
+      <video
+        className="h-full w-full object-cover absolute inset-0"
+        src={VideoBg}
+        autoPlay
+        muted
+        loop
+      ></video>
       <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
       <div className="timer-container absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white z-20">
         <h1 className="text-4xl font-bold ">Launching Soon!</h1>
-        <p className="text-xl mb-4">टेस्ट सीरीज जल्द ही लॉन्च होने जा रही है।</p>
+        <p className="text-xl mb-4">
+          टेस्ट सीरीज जल्द ही लॉन्च होने जा रही है।
+        </p>
         <div className="countdown grid grid-cols-4 gap-4">
           <div className="time-unit">
             <span className="text-3xl font-bold">{timeRemaining.days}</span>

70342d729b6f89e4c2d73595e0977f95e9cfc986
Ishu Singh <104965112+ishuoncode@users.noreply.github.com>
2024-02-02 15:58:36 +0530

    add link protection

--- frontend/src/Components/test/test-components/StartTest.jsx
+++ frontend/src/Components/test/test-components/StartTest.jsx
@@ -12,6 +12,30 @@ export function StartTest({ userData }) {
   const [allow, setAllow] = useState(false);
   const { id } = useParams();
   const [newdata,setNewData]=useState(null)
+  const [open,setOpen]=useState(true)
+
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/test/${id}`);
+   
+        if(Date.now() >= response.data.data.test.mainend){
+          
+          setOpen(false)
+        }
+        
+      } catch (error) {
+        console.error("Error fetching test data:", error);
+      }
+    };
+
+    fetchData();
+  }, [id, setOpen]);
+
+
+
 
   // Retrieve data from local storage when the component mounts
   useEffect(() => {
@@ -24,6 +48,10 @@ export function StartTest({ userData }) {
       setSelectedDistrict(storedSelectedDistrict);
     }
   }, []);
+
+
+ 
+
 
   const handleStartTest = async () => {
     const userid = userData.user._id;
@@ -81,76 +109,80 @@ export function StartTest({ userData }) {
   
   return (
     <>
-      {allow ? (
-        <div className="py-[8rem] text-center">
-          you already gave the test
-          <br />
-        </div>
-      ) : (
-        <>
-          {liveTest ? (
-            <LiveTest userData={newdata} />
-          ) : (
-            <div className="mx-auto py-24 flex justify-center items-center">
-              <div className="w-96 bg-white rounded-lg p-6 shadow-md">
-                <div className="text-2xl font-bold mb-4">
-                {localStorage.getItem("testname")}
-                </div>
-                <div className="text-gray-500 text-sm mb-4">
-                  {localStorage.getItem("testdate")}
-                </div>
-                <form className="grid gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <label htmlFor="number" className="text-sm text-gray-600">
-                      Phone Number
-                    </label>
-                    <input
-                      id="number"
-                      type="text"
-                      required="true"
-                      placeholder="Enter your Phone number"
-                      className="border border-gray-300 p-2 rounded-md"
-                      value={phoneNumber}
-                      onChange={(e) => setPhoneNumber(e.target.value)}
-                    />
+      {open ? (
+        allow ? (
+          <div className="py-[8rem] text-center">
+            You already gave the test
+            <br />
+          </div>
+        ) : (
+          <>
+            {liveTest ? (
+              <LiveTest userData={newdata} />
+            ) : (
+              <div className="mx-auto py-24 flex justify-center items-center">
+                <div className="w-96 bg-white rounded-lg p-6 shadow-md">
+                  <div className="text-2xl font-bold mb-4">
+                    {localStorage.getItem("testname")}
                   </div>
-                  <div className="flex flex-col space-y-1.5">
-                    <label
-                      htmlFor="districts"
-                      className="text-sm text-gray-600"
+                  <div className="text-gray-500 text-sm mb-4">
+                    {localStorage.getItem("testdate")}
+                  </div>
+                  <form className="grid gap-4">
+                    <div className="flex flex-col space-y-1.5">
+                      <label htmlFor="number" className="text-sm text-gray-600">
+                        Phone Number
+                      </label>
+                      <input
+                        id="number"
+                        type="text"
+                        required={true}
+                        placeholder="Enter your Phone number"
+                        className="border border-gray-300 p-2 rounded-md"
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                      />
+                    </div>
+                    <div className="flex flex-col space-y-1.5">
+                      <label
+                        htmlFor="districts"
+                        className="text-sm text-gray-600"
+                      >
+                        Districts
+                      </label>
+                      <input
+                        id="districts"
+                        type="text"
+                        required={true}
+                        placeholder="Enter your District name"
+                        className="border border-gray-300 p-2 rounded-md"
+                        value={selectedDistrict}
+                        onChange={(e) => setSelectedDistrict(e.target.value)}
+                      />
+                    </div>
+                  </form>
+                  <div className="flex justify-between mt-6">
+                    <Link to="/test">
+                      <button className="bg-gray-300 px-4 py-2 rounded-md text-gray-700 hover:bg-gray-400">
+                        Cancel
+                      </button>
+                    </Link>
+                    <button
+                      onClick={handleStartTest}
+                      className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
                     >
-                      Districts
-                    </label>
-                    <input
-                      id="districts"
-                      type="text"
-                      required="true"
-                      placeholder="Enter your District name"
-                      className="border border-gray-300 p-2 rounded-md"
-                      value={selectedDistrict}
-                      onChange={(e) => setSelectedDistrict(e.target.value)}
-                    />
- 
-                  </div>
-                </form>
-                <div className="flex justify-between mt-6">
-                  <Link to="/test">
-                    <button className="bg-gray-300 px-4 py-2 rounded-md text-gray-700 hover:bg-gray-400">
-                      Cancel
+                      Start
                     </button>
-                  </Link>
-                  <button
-                    onClick={handleStartTest}
-                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-                  >
-                    Start
-                  </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
-        </>
+            )}
+          </>
+        )
+      ) : (
+        <div className="pt-[10rem] text-center mb-[5rem]">Opps! Test Ended 😟. <br/>Donot try to be oversmart😎.</div>
       )}
     </>
   );
+  
 }
